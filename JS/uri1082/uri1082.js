f254ccad4ca2e0dd72adb1a82ebba343c699e0c1
Army <keymasterviriya1150@gmail.com>
2019-08-14 01:18:41 +0700

    solve 1082

--- JS/uri1082/uri1082.js
+++ JS/uri1082/uri1082.js
@@ -1,34 +1,12 @@
 const input = require('fs').readFileSync('uri1082/input.txt', 'utf8');
 let lines = input.split('\n');
-class Queue {
-    // Array is used to implement a Queue 
-    constructor() {
-        this.items = [];
-    }
-    enqueue(element) {
-        this.items.push(element);
-    }
-    dequeue() {
-        return this.isEmpty() ? "Underflow" : this.items.shift();
-    }
-    front() {
-        return this.isEmpty() ? "No elements in Queue" : this.items[0];
-    }
-    isEmpty() {
-        return this.items.length == 0;
-    }
-    printQueue() {
-        return this.items.forEach((item, str = "") => str += item + " ");
-    }
-}
-
-
 class Graph {
     constructor(noOfVertices) {
         this.noOfVertices = noOfVertices;
-        this.AdjMetrix = Array(TotalOfVertex).fill(null).map(() => Array(TotalOfVertex).fill(0));
+        this.AdjMetrix = Array(noOfVertices).fill(null).map(() => Array(noOfVertices).fill(0));
         this.path = new Array();
-        this.visited = new Array(TotalOfVertex).fill(false);
+        this.visited = new Array(noOfVertices).fill(false);
+        this.connect = new Array();
     }
     getIndexBy(char) {
         return char.charCodeAt(0) - 97;
@@ -55,33 +33,29 @@ class Graph {
     }
 
     dfs(vertex1) {
-        let connect = new Array();
-        connect.push(this.getCharBy(vertex1));
+        this.connect.push(this.getCharBy(vertex1));
         this.path.push(vertex1)
         this.visited[vertex1] = true
         for (let i = 0; i < this.noOfVertices; i++) {
             if (!this.visited[i]) {
                 if (this.AdjMetrix[vertex1][i] === 1) {
-                    connect.push(this.getCharBy(i));
-                    console.log(`AdjMetrix=${this.AdjMetrix[vertex1][i]} vertex1=${this.getCharBy(vertex1)} i=${this.getCharBy(i)}`);
-                    console.log(`${this.getCharBy(vertex1)}=>${this.getCharBy(i)}`);
                     this.dfs(i)
                 }
             }
         }
-        return connect;
     }
 
     dfs_runner(TotalOfVertex) {
         let countConnect = 0;
         for (let v = 0; v < TotalOfVertex; v++) {
             if (!this.visited[v]) {
+                this.connect = new Array();
                 countConnect++
-                let connect = this.dfs(v);
-                console.log(`${connect},`);
+                this.dfs(v);
+                console.log(`${this.connect.sort()},`);
             }
         }
-        console.log(`${countConnect} connected components`);
+        console.log(`${countConnect} connected components\n`);
     }
 }
 
@@ -89,17 +63,19 @@ class Graph {
 
 let Number = 0
 let Testcaee = lines[Number++];
-let contaners = lines[Number].split(` `);
-let TotalOfVertex = parseInt(contaners[0])
-let TotalOfEdge = parseInt(contaners[1])
-var g = new Graph(TotalOfVertex);
-
-const start = ++Number
-for (let i = start; i < TotalOfEdge + start; i++) {
-    const vertex = lines[i].split(' ');
-    let v1 = vertex[0].toString().replace(/\n|\r/g, "")
-    let v2 = vertex[1].toString().replace(/\n|\r/g, "")
-    g.addEdge(g.getIndexBy(v1), g.getIndexBy(v2))
-    Number++;
+for(let t =1;t<=Testcaee;t++){
+    console.log(`Case #${t}:`);
+    let contaners = lines[Number].split(` `);
+    let TotalOfVertex = parseInt(contaners[0])
+    let TotalOfEdge = parseInt(contaners[1])
+    var g = new Graph(TotalOfVertex);
+    const start = ++Number
+    for (let i = start; i < TotalOfEdge + start; i++) {
+        const vertex = lines[i].split(' ');
+        let v1 = vertex[0].toString().replace(/\n|\r/g, "")
+        let v2 = vertex[1].toString().replace(/\n|\r/g, "")
+        g.addEdge(g.getIndexBy(v1), g.getIndexBy(v2))
+        Number++;
+    }
+    g.dfs_runner(TotalOfVertex)
 }
-g.dfs_runner(TotalOfVertex)

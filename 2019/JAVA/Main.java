accd8ce6706caf2698f52ba2d7d73d0825a3720b
Army <keymasterviriya1150@gmail.com>
2019-08-23 10:14:03 +0700

    change name folder

--- 2019/JAVA/Main.java
+++ 2019/JAVA/Main.java
@@ -4,15 +4,8 @@ class Main {
     public static void main(String args[]) throws Exception {
         File file = new File("input.txt"); 
         BufferedReader br = new BufferedReader(new FileReader(file)); 
-        
-
-            String age =  br.readLine(); 
-            String[] g = age.split(" ");
-            System.out.println("age: "+g[1]); 
-
-    
-  
-
-        
+            String line =  br.readLine(); 
+            String[] n = line.split(" ");
+            System.out.println("age: "+n[1]); 
     }
 }

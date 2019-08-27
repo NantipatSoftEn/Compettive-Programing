ba20bfd097a05546f3ba6d2d85459fcf462c66d8
Army <keymasterviriya1150@gmail.com>
2019-08-27 11:25:23 +0700

    Runtime error 1931

--- 2019/uri1931/uri1931.js
+++ 2019/uri1931/uri1931.js
@@ -1,3 +1,6 @@
+const input = require('fs').readFileSync('uri1931/input.txt', 'utf8');
+let lines = input.split('\n');
+
 class PriorityQueue {
     constructor() {
         this.collection = [];
@@ -38,13 +41,17 @@ class Graph {
         this.path = {};
         this.visited = new Array(TotalOfVertex).fill(false);
         this.total = TotalOfVertex;
+        this.adjList = {};
+        for (let i = 1; i < TotalOfVertex; i++) {
+            this.addNode(i)
+        }
     }
     addEdge(src, dist, w) {
         this.adj[src][dist] = w;
         this.adj[dist][src] = w;
     }
 
-    dks(src, dist, adj2) {
+    dks(src, dist) {
         let pq = new PriorityQueue();
         this.weight[src] = 0;
         pq.enqueue([src, 0])
@@ -54,15 +61,15 @@ class Graph {
             let currentWeight = top[1];
             if (this.weight[currentNode] > currentWeight)
                 continue;
-            this.calculateWeight(currentNode, currentWeight, pq, adj2);
+            this.calculateWeight(currentNode, currentWeight, pq);
         }
         return this.weight[dist];
     }
-    calculateWeight(currentNode, currentWeight, pq, adj2) {
-        adj2[currentNode].forEach(element => {
-            if (currentWeight + element.w < this.weight[element.n]) {
-                this.weight[element.n] = currentWeight + element.w;
-                pq.enqueue([element.n,  this.weight[element.n]]);
+    calculateWeight(currentNode, currentWeight, pq) {
+        this.adjList[currentNode].forEach(to => {
+            if (currentWeight + to.w < this.weight[to.n]) {
+                this.weight[to.n] = currentWeight + to.w;
+                pq.enqueue([to.n, this.weight[to.n]]);
             }
         });
 
@@ -78,37 +85,44 @@ class Graph {
         }
         return `Path is ${path}`
     }
+    addNode(node) {
+        this.adjList[node] = []
+    }
 
-
-}
-const dfs = (adj1, adj2, visited, src) => {
-    if (!visited[src]) {
-        visited[src] = true;
-        for (let i = 1; i < adj1[src].length; i++) {
-            for (let j = 1; j < adj1[i].length; j++) {
-                adj2[src].push({
-                    n: j,
-                    w: adj1[src][i] + adj1[i][j]
-                })
-                dfs(adj1, adj2, visited, j)
+    dfs(src) {
+        if (!this.visited[src]) {
+            this.visited[src] = true;
+            for (let i = 0; i < this.adj[src].length; i++) {
+                for (let j = 1; j < this.adj[i].length; j++) {
+                    this.adjList[src].push({
+                        n: j,
+                        w: this.adj[src][i] + this.adj[i][j]
+                    })
+                    this.dfs(j)
+                }
             }
         }
     }
+
 }
 
-const addNode = (adj2, node) => {
-    adj2[node] = []
+let n = 0;
+const contaner = lines[n++].split(` `);
+const v = parseInt(contaner[0]);
+const e = parseInt(contaner[1]);
+let g = new Graph(v + 1);
+for (let i = n; i <= e; i++) {
+    let value = lines[n++].split(` `);
+    let src = parseInt(value[0]);
+    let dist = parseInt(value[1]);
+    let w = parseInt(value[2]);
+    g.addEdge(src, dist, w)
 }
-let g = new Graph(5);
-g.addEdge(1, 2, 2)
-g.addEdge(2, 3, 1)
-g.addEdge(2, 4, 10)
-g.addEdge(3, 4, 6)
-
-let adj2 = {}
-for (let i = 1; i <= 5; i++) {
-    addNode(adj2, i)
+g.dfs(1)
+const  result  = g.dks(1, v);
+if (result !== Infinity) {
+    console.log(result);
+} else {
+    console.log(`-1`);
+    
 }
-let visited = new Array(5).fill(false);
-dfs(g.adj, adj2, visited, 1)
-console.log(g.dks(1, 4, adj2));

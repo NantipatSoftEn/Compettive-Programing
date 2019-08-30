15f839a0f189b25c65f9bdfc30e4f43a3dab418a
Army <keymasterviriya1150@gmail.com>
2019-08-30 17:59:04 +0700

    template

---
+++ Upgrade/template.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define debug(x) cout << #x " = " << (x) << endl
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout << "gg gyt" << endl;
+    return 0;
+
+}

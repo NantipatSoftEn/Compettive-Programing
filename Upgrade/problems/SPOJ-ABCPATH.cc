9f9ed45e1a575eb36ccc389406d60158a62a7574
Army <keymasterviriya1150@gmail.com>
2019-09-25 11:19:05 +0700

    throws

--- Upgrade/problems/SPOJ-ABCPATH.cc
+++ Upgrade/problems/SPOJ-ABCPATH.cc
@@ -1,106 +1,97 @@
+#include<cstdio>
+#include<iostream>
+#include<string>
+#include<cstdlib>
+#include<queue>
+#include<stack>
 #include <bits/stdc++.h>
-
+#define in_range(x, y, r, c) (x < r && y < c && x >= 0 && y >= 0)
 using namespace std;
 
-#define debug(x) cout << #x " = " << (x) << endl
+char mat[51][51];
+int r, c, dis[51][51];
 
-const long long inf = 1000000000;
-int n, m;
-int  adj[51][51];
-int dist[51][51];
-bool IsRange(int x, int y)
-{
-    return (x < n) && (x >= 0) && (y < m) && (y >= 0);
+void dfs(int i, int j, int dist){
+	int x, y;
+	dis[i][j] = dist+1;
+	
+	x = i-1;
+	y = j-1;
+	if(in_range(x, y, r, c) && mat[x][y] == mat[i][j] + 1 && dis[x][y] < dis[i][j] + 1){
+		dfs(x, y, dis[i][j]);
+	}
+	
+	x = i-1;
+	y = j;
+	if(in_range(x, y, r, c) && mat[x][y] == mat[i][j] + 1 && dis[x][y] < dis[i][j] + 1){
+		dfs(x, y, dis[i][j]);
+	}
+	
+	x = i-1;
+	y = j+1;
+	if(in_range(x, y, r, c) && mat[x][y] == mat[i][j] + 1 && dis[x][y] < dis[i][j] + 1){
+		dfs(x, y, dis[i][j]);
+	}
+	
+	x = i;
+	y = j-1;
+	if(in_range(x, y, r, c) && mat[x][y] == mat[i][j] + 1 && dis[x][y] < dis[i][j] + 1){
+		dfs(x, y, dis[i][j]);
+	}
+	
+	x = i;
+	y = j+1;
+	if(in_range(x, y, r, c) && mat[x][y] == mat[i][j] + 1 && dis[x][y] < dis[i][j] + 1){
+		dfs(x, y, dis[i][j]);
+	}
+	
+	x = i+1;
+	y = j-1;
+	if(in_range(x, y, r, c) && mat[x][y] == mat[i][j] + 1 && dis[x][y] < dis[i][j] + 1){
+		dfs(x, y, dis[i][j]);
+	}
+	
+	x = i+1;
+	y = j;
+	if(in_range(x, y, r, c) && mat[x][y] == mat[i][j] + 1 && dis[x][y] < dis[i][j] + 1){
+		dfs(x, y, dis[i][j]);
+	}
+	
+	x = i+1;
+	y = j+1;
+	if(in_range(x, y, r, c) && mat[x][y] == mat[i][j] + 1 && dis[x][y] < dis[i][j] + 1){
+		dfs(x, y, dis[i][j]);
+	}
+	
 }
-void floodFill(int x, int y, int countDist)
-{
-    int movex, movey;
-    dist[x][y] = countDist + 1;
-    movex = x - 1;
-    movey = y - 1;
-    if (IsRange(movex, movey) && adj[movex][movex] == adj[x][y] + 1 && dist[movex][movex] < dist[x][y] + 1)
-        floodFill(movex, movey, dist[x][y]);
-    movex = x - 1;
-    movey = y;
-    if (IsRange(movex, movey) && adj[movex][movey] == adj[x][y] + 1 && dist[movex][movey] < dist[x][y] + 1)
-        floodFill(movex, movey, dist[x][y]);
 
-    movex = x - 1;
-    movey = y + 1;
-    if (IsRange(movex, movey) && adj[movex][movey] == adj[x][y] + 1 && dist[movex][movey] < dist[x][y] + 1)
-        floodFill(movex, movey, dist[x][y]);
-
-    movex = x;
-    movey = y - 1;
-    if (IsRange(movex, movey) && adj[movex][movey] == adj[x][y] + 1 && dist[movex][movey] < dist[x][y] + 1)
-        floodFill(movex, movey, dist[x][y]);
-
-    movex = x;
-    movey = y + 1;
-    if (IsRange(movex, movey) && adj[movex][movey] == adj[x][y] + 1 && dist[movex][movey] < dist[x][y] + 1)
-        floodFill(movex, movey, dist[x][y]);
-
-    movex = x + 1;
-    movey = y - 1;
-    if (IsRange(movex, movey) && adj[movex][movey] == adj[x][y] + 1 && dist[movex][movey] < dist[x][y] + 1)
-        floodFill(movex, movey, dist[x][y]);
-
-    movex = x + 1;
-    movey = y;
-    if (IsRange(movex, movey) && adj[movex][movey] == adj[x][y] + 1 && dist[movex][movey] < dist[x][y] + 1)
-        floodFill(movex, movey, dist[x][y]);
-
-    movex = x + 1;
-    movey = y + 1;
-    if (IsRange(movex, movey) && adj[movex][movey] == adj[x][y] + 1 && dist[movex][movey] < dist[x][y] + 1)
-        floodFill(movex, movey, dist[x][y]);
-}
-int main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    freopen("input.txt", "r", stdin);
-    int tc = 1;
-    int max;
-    while (1)
-    {
-        cin >> n >> m;
-        if (n==0 && m==0)
-            break;
-        for (size_t i = 0; i < n; i++)
-        {
-            for (size_t j = 0; j < m; j++)
-            {
-                char c;
-                cin >> c;
-                adj[i][j] = (int)c-64;
-                dist[i][j] = 0;
-            }
-        }
-
-        for (size_t i = 0; i < n; i++)
-        {
-            for (size_t j = 0; j < m; j++)
-            {
-                if (adj[i][j] == 1 && dist[i][j] == 0)
-                {
-                    floodFill(i, j, 0);
-                }
-            }
-        }
-
-        max = dist[0][0];
-        for (size_t i = 0; i < n; i++)
-        {
-            for (size_t j = 0; j < m; j++)
-            {
-                if (max < dist[i][j])
-                    max = dist[i][j];
-            }
-        }
-        printf("Case %d: %d\n", tc, max);
-
-        tc++;
-    }
+int main(){
+	
+	int max, tc=0;
+	freopen("input.txt", "r", stdin);
+	while(true){
+		scanf("%d%d", &r, &c);
+		if(!r && !c)
+			break;
+		tc++;
+		for(int i=0;i<r;i++){
+			scanf("%s", mat[i]);
+			for(int j=0;j<c;j++)
+				dis[i][j] = 0;
+		}
+		for(int i=0;i<r;i++){
+			for(int j=0;j<c;j++){
+				if(mat[i][j] == 'A' && dis[i][j] == 0)
+					dfs(i, j, 0);
+			}
+		}
+		max = dis[0][0];
+		for(int i=0;i<r;i++){
+			for(int j=0;j<c;j++)
+				if(max < dis[i][j])
+					max = dis[i][j];
+		}
+		printf("Case %d: %d\n", tc, max);
+	}
     return 0;
 }

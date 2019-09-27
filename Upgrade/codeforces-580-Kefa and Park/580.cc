a18d1240fe33842e96a76c4e44bdf5676f116b0c
Army <keymasterviriya1150@gmail.com>
2019-09-27 10:21:46 +0700

    resolve

--- Upgrade/codeforces-580-Kefa and Park/580.cc
+++ Upgrade/codeforces-580-Kefa and Park/580.cc
@@ -1,72 +1,46 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 
 using namespace std;
-
-#define debug(x) cout << #x " = " << (x) << endl
-
-const long long inf = 1000000000;
 #define MAX 10000001
-typedef vector<vector<int>> graph;
-graph adj;
-vector<bool> visited;
-int n, m;
-int cats[MAX];
-int result = 0;
-void printGraph(int n)
-{
-    for (size_t v = 0; v < n; v++)
-    {
-        for (auto &u : adj[v])
-        {
-            printf("start=%d dest=%d\n", v, u);
-        }
-    }
-}
-void dfs(int v, int sumCat)
+int isCat[MAX];
+bool visited[MAX];
+vector<vector<int> > graph(MAX);
+int n , m , ans = 0;
+void dfs(int u , int sum_cat)
 {
     int sum = 0;
-    visited[v] = true;
-    if (sumCat > m)
+    visited[u] = true;
+    if(sum_cat > m)
         return;
+
     bool ok = false;
-    for (auto u : adj[v])
+    for(int v = 0 ; v < graph[u].size() ; v++)
     {
-        if (!visited[u])
+        if(!visited[graph[u][v]])
         {
-            if (cats[u])
-                sum = sumCat + 1;
-            else
-                sum = 0;
-            dfs(u, sum);
+            //cout << u << " " << v << endl;
+            isCat[graph[u][v]] ? sum = sum_cat + 1 : sum = 0;
+            dfs(graph[u][v] , sum);
             ok = true;
         }
     }
-    if (!ok)
-        result++;
+    if(!ok)
+        ans++;
 }
-
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+    int x , y;
+    cin >> n >> m;//cout << n << m;
+    for(int i = 1 ; i <= n ; i++)
+        cin >> isCat[i];
 
-    freopen("input.txt", "r", stdin);
-    cin >> n >> m;
-    adj.resize(n + 1);
-    for (size_t i = 1; i < n + 1; i++)
+    for(int i = 1; i < n ; i++)
     {
-        cin >> cats[i];
+        cin >> x >> y;
+        graph[x].push_back(y);
+        graph[y].push_back(x);
     }
-
-    visited.assign(n + 1, false);
-    for (int i = 0; i < n - 1; i++)
-    {
-        int u, v;
-        cin >> u >> v;
-        adj[u].push_back(v);
-    }
-    dfs(1, cats[1]);
-    cout << result << endl;
-    //printGraph(n);
+    dfs(1 , isCat[1]);
+    cout << ans;
     return 0;
 }
